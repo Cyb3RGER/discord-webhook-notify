c79db5a416063b6e50a3b3dea5a38d35f214521f
Cyb3RGER <niklasm.cyber@gmail.com>
2022-01-30 17:19:27 +0100

    change getDefaultDescription for payload action published on release event

--- index.js
+++ index.js
@@ -6,88 +6,96 @@ const webhook = require("webhook-discord");
 const default_avatarUrl = "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png";
 const default_username = "GitHub";
 const default_colors = {
-    info: '#00ff00',
-    warn: '#ff9900',
-    error: '#ff0000'
+	info: '#00ff00',
+	warn: '#ff9900',
+	error: '#ff0000'
 }
 const long_severity = {
-    info: "Informational",
-    warn: "Warning",
-    error: "Error"
+	info: "Informational",
+	warn: "Warning",
+	error: "Error"
 }
 
 async function getDefaultDescription() {
-    const context = github.context;
-    const payload = context.payload;
+	const context = github.context;
+	const payload = context.payload;
 
-    switch(github.context.eventName) {
-    case 'push':
-        return `- **Event:** ${context.eventName}\n`
-            + `- **Repo:** ${payload.repository.full_name}\n`
-            + `- **Ref:** ${payload.ref}\n`
-            + `- **Workflow:** ${context.workflow}\n`
-            + `- **Author:** ${payload.head_commit.author.name}\n`
-            + `- **Committer:** ${payload.head_commit.committer.name}\n`
-            + `- **Pusher:** ${payload.pusher.name}\n`
-            + `- **Commit URL:** ${payload.head_commit.url}\n`
-            + `- **Commit Message:** ${payload.head_commit.message}\n`
-            ;
-    case 'release':
-        return `- **Event:** ${context.eventName}\n`
-            + `- **Repo:** ${payload.repository.full_name}\n`
-            + `- **Action:** ${payload.action}\n`
-            + `- **Name**: ${payload.release.name}\n`
-            + `- **Author:** ${payload.release.author.login}\n`
-            + `- **Tag:** ${payload.release.tag_name}`
-            + (payload.release.prerelease ? ' (pre-release)' : '')
-            + '\n'
-            + `- **Url:** ${payload.release.url}`
-            ;
-    default:
-        return `- **Event:** ${context.eventName}\n`
-            + `- **Repo:** ${payload.repository.full_name}\n`;
-    }
+	switch (github.context.eventName) {
+		case 'push':
+			return `- **Event:** ${context.eventName}\n`
+				+ `- **Repo:** ${payload.repository.full_name}\n`
+				+ `- **Ref:** ${payload.ref}\n`
+				+ `- **Workflow:** ${context.workflow}\n`
+				+ `- **Author:** ${payload.head_commit.author.name}\n`
+				+ `- **Committer:** ${payload.head_commit.committer.name}\n`
+				+ `- **Pusher:** ${payload.pusher.name}\n`
+				+ `- **Commit URL:** ${payload.head_commit.url}\n`
+				+ `- **Commit Message:** ${payload.head_commit.message}\n`
+				;
+		case 'release':
+			if (payload.action == "published")
+				return `**A new ${(payload.release.prerelease ? 'pre-release' : 'release')} of ${payload.repository.name} has been published**\n`
+					+ `- **${payload.release.name}**\n`
+					+ `- **Repo:** ${payload.repository.full_name}\n`
+					+ `- **Tag:** ${payload.release.tag_name}`
+					+ `- **Url:** ${payload.release.html_url}`
+					;
+			else
+				return `- **Event:** ${context.eventName}\n`
+					+ `- **Repo:** ${payload.repository.full_name}\n`
+					+ `- **Action:** ${payload.action}\n`
+					+ `- **Name**: ${payload.release.name}\n`
+					+ `- **Author:** ${payload.release.author.login}\n`
+					+ `- **Tag:** ${payload.release.tag_name}`
+					+ (payload.release.prerelease ? ' (pre-release)' : '')
+					+ '\n'
+					+ `- **Url:** ${payload.release.html_url}`
+					;
+		default:
+			return `- **Event:** ${context.eventName}\n`
+				+ `- **Repo:** ${payload.repository.full_name}\n`;
+	}
 }
 
 async function run() {
-    try {
-        const webhookUrl = core.getInput('webhookUrl').replace("/github", "");
-        if (!webhookUrl) {
-            core.setFailed("The webhookUrl was not provided. For security reasons the secret URL must be provided "
-                           + "in the action yaml using a context expression and can not be read as a default.");
-            return;
-        }
-        const severity = core.getInput('severity');
-        const description = core.getInput('description');
-        const details = core.getInput('details');
-        const footer = core.getInput('footer');
-        const text = core.getInput('text');
-        const username = core.getInput('username');
-        const color = core.getInput('color');
-        const avatarUrl = core.getInput('avatarUrl');
-        
-        const context = github.context;
-        const obstr = JSON.stringify(context, undefined, 2)
-        core.debug(`The event github.context: ${obstr}`);
+	try {
+		const webhookUrl = core.getInput('webhookUrl').replace("/github", "");
+		if (!webhookUrl) {
+			core.setFailed("The webhookUrl was not provided. For security reasons the secret URL must be provided "
+				+ "in the action yaml using a context expression and can not be read as a default.");
+			return;
+		}
+		const severity = core.getInput('severity');
+		const description = core.getInput('description');
+		const details = core.getInput('details');
+		const footer = core.getInput('footer');
+		const text = core.getInput('text');
+		const username = core.getInput('username');
+		const color = core.getInput('color');
+		const avatarUrl = core.getInput('avatarUrl');
 
-        const hook = new webhook.Webhook(webhookUrl);
+		const context = github.context;
+		const obstr = JSON.stringify(context, undefined, 2)
+		core.debug(`The event github.context: ${obstr}`);
 
-        core.info(`${username} ${avatarUrl} ${color} ${description} ${details} ${footer} ${text}`)
+		const hook = new webhook.Webhook(webhookUrl);
 
-        const msg = new webhook.MessageBuilder()
-                        .setName(username || default_username)
-                        .setAvatar(avatarUrl || default_avatarUrl)
-                        .setColor(color || default_colors[severity])
-                        .setDescription((description || await getDefaultDescription()) + "\n" + details)
-                        .setFooter(footer || ("Severity: " + long_severity[severity]))
-                        .setText(text)
-                        .setTime();
+		core.info(`${username} ${avatarUrl} ${color} ${description} ${details} ${footer} ${text}`)
 
-        hook.send(msg);
+		const msg = new webhook.MessageBuilder()
+			.setName(username || default_username)
+			.setAvatar(avatarUrl || default_avatarUrl)
+			.setColor(color || default_colors[severity])
+			.setDescription((description || await getDefaultDescription()) + "\n" + details)
+			.setFooter(footer || ("Severity: " + long_severity[severity]))
+			.setText(text)
+			.setTime();
 
-    } catch (error) {
-        core.setFailed(error.message);
-    }
+		hook.send(msg);
+
+	} catch (error) {
+		core.setFailed(error.message);
+	}
 }
 
 run();
