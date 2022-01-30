b9efdfaedbaddd6a0510433a03f906d39acd7d1c
Cyb3RGER <niklasm.cyber@gmail.com>
2022-01-30 18:00:21 +0100

    added body to release/published getDefaultDescription

--- index.js
+++ index.js
@@ -91,6 +91,7 @@ async function getDefaultDescription() {
 		case 'release':
 			if (payload.action == "published")
 				return `A new ${(payload.release.prerelease ? 'pre-release' : 'release')} of ${payload.repository.name} has been published: **${payload.release.name}**\n`
+					+ `${payload.release.body}\n`
 					+ `**It can be found here:** ${payload.release.html_url}`
 					;
 			else
